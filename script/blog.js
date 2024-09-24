268d9365ef06b70373f2429ea1a41eb520318a46
Schr0Sm1ley <radiantremel444@gmail.com>
2024-09-24 22:48:33 +0600

    Finalizing the project

--- script/blog.js
+++ script/blog.js
@@ -1,10 +1,12 @@
 // switches to the blog page
-function blogPage() {
+function blogPage(event) {
+    event.preventDefault();
     window.location.href = '/HopeLink-Empowering-Donations/Blog.html';
 }
 
 
 // switches to the home page
-function homePage() {
+function homePage(event) {
+    event.preventDefault();
     window.location.href = '/HopeLink-Empowering-Donations/';
 }
